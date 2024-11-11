8c8c59147df0ddfcc405886628fc7aa342dbfbf0
Clara Gadelho <clara.moreira.2000@gmail.com>
2024-11-11 11:23:51 +0000

    product code directly

--- libs/proxy/llmstudio_proxy/register_usage_aws.py
+++ libs/proxy/llmstudio_proxy/register_usage_aws.py
@@ -6,7 +6,7 @@ client = boto3.client('meteringmarketplace', region_name='us-east-1')
 def registerUsage():
     try:
         response = client.register_usage(
-            ProductCode=os.environ["AWS_PROD_CODE"],
+            ProductCode="7d25fgis4ph4jqywofjybfr2r",
             PublicKeyVersion=1
         )
         print('Response from RegisterUsage API call ' + str(response))
